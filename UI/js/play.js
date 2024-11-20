ee9022de3f16d7dee40a90f2b262c7de14a362c1
Martins Aloba <aloba.martins@gmail.com>
2024-11-19 19:42:35 -0500

    add production api

--- UI/js/play.js
+++ UI/js/play.js
@@ -1,4 +1,5 @@
-const url = "http://localhost:3000/api/v1";
+// const url = "http://localhost:3000/api/v1";
+const url = "https://guhuza.onrender.com/api/v1";
 
 // Check the user's progress from local storage
 window.onload = async function () {
